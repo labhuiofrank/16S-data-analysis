01fb6636b7d9a4fb341f4be92a5b76ce2a347ac9
Cédric Arisdakessian <carisdak@hawaii.edu>
2022-02-18 11:52:34 -1000

    added other ordination method and biplot

--- R/ordination.R
+++ R/ordination.R
@@ -1,28 +1,38 @@
 library(phyloseq)
 library(vegan)
 
-#' Wrapper around phyloseq's ordinate function to compute NMDS and include metadata
-#' This function needs to be updated with more specific behaviour for other types
-#' of ordination methods (PCoA, CCA, ...)
+#' Wrapper around phyloseq's ordinate function
+#' Adds the metadata to the component scores
 #'
 #' @param ps Phyloseq object. Needs to contain a phylogenetic tree if the distance matrix is unifrac-based
 #' @param method Ordinaton method. See `ordinate()` function to see all the available methods
 #' @param ... Extra arguments to provide to the underlying ordination function
-#' @return Dataframe with first 2 components and metadata information
+#' @return List with ordination model, dataframe with first 2 components and metadata information, and biplot data if supervised
 #' @examples
-#' run_nmds(ps, method="NMDS", distance="bray", trymax=300)
-#' run_nmds(ps, method="PCoA")
-run_nmds <- function(ps, method="NMDS", ...) {
-    
+#' run_ordination(ps, method="NMDS", distance="bray", trymax=300)
+#' run_ordination(ps, method="PCoA")
+#' run_ordination(ps, "CCA", formula=~ DO + pH + SPC)
+run_ordination <- function(ps, method="NMDS", ...) {
+
     ord.mod <- ordinate(ps, method=method, ...)
 
-    if (method == "NMDS") {
-        print(sprintf("stress=%s, converged=%s", ord.mod$stress, ord.mod$converged))
-        components <- scores(ord.mod)
+    if (method == "PCoA") {
+        components <- ord.mod$vectors
+    } else {
+        components <- scores(ord.mod, display="sites")
     }
+    components <- as.data.frame(components)
+    n_components <- dim(components)[2]
 
     df <- sample_data(ps)[rownames(components),]
-    df[, sprintf("%s%s", method, 1:2)] <- components[, 1:2]
+    df[, sprintf("%s%s", method, 1:2)] <- components[, 1:n_components]
+
+    results <- list(data=df, model=ord.mod)
     
-    return(df)
+    if (method %in% c("CCA", "RDA")) {
+        ## Supervised --> extract the vectors for biplot
+        results$arrows <- scores(ord.mod, display="bp")
+    }
+    
+    return(results)
 }
